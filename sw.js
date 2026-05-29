23830cfa748dcfec8ecbaf50c5c25b89a3d00285
xiao0823@qq.com <xiao0823@qq.com>
2026-05-29 17:51:30 +0800

    chore: update service worker cache strategy

--- sw.js
+++ sw.js
@@ -1,11 +1,24 @@
-const CACHE_NAME = "game2048-pwa-v1";
+const CACHE_NAME = "game2048-pwa-v2";
 const ASSETS = [
   "./",
   "./index.html",
   "./2048小游戏.html",
+  "./styles.css",
+  "./game.js",
   "./manifest.webmanifest",
   "./icon-192.svg",
-  "./icon-512.svg"
+  "./icon-512.svg",
+  "./assets/source/2.png",
+  "./assets/source/4.png",
+  "./assets/source/8.png",
+  "./assets/source/16.png",
+  "./assets/source/32.png",
+  "./assets/source/64.png",
+  "./assets/source/128.png",
+  "./assets/source/256.png",
+  "./assets/source/512.png",
+  "./assets/source/1204.png",
+  "./assets/source/2048.png"
 ];
 
 self.addEventListener("install", (event) => {
@@ -28,6 +41,25 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(event.request.url);
+  const isSameOrigin = requestUrl.origin === self.location.origin;
+  const isFreshResource =
+    event.request.mode === "navigate" ||
+    (isSameOrigin && [".html", ".css", ".js", ".webmanifest"].some((ext) => requestUrl.pathname.endsWith(ext)));
+
+  if (isFreshResource) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+          return response;
+        })
+        .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html"))),
+    );
     return;
   }
 
